2089e012631cda1fa6ae0017d55b5af3f9925333
Renato Diniz <renatodiniz25@gmail.com>
2023-12-17 13:40:31 -0300

    Implement tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{command, Arg, ArgAction};
+// use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg(Arg::new("no_newline").help("Do not print newline").short('n').long("no_newline").action(ArgAction::SetTrue))
+        .arg(Arg::new("input").help("Inputs to print").default_value("").action(ArgAction::Append))// .arg(arg!(--brand <VALUE>))
+        .get_matches();
+
+    let no_newline = *matches.get_one::<bool>("no_newline").unwrap();
+    let text = &matches
+        .get_many::<String>("input")
+        .unwrap()
+        .map(|v| v.as_str())
+        .collect::<Vec<_>>().join(" ");
+
+    if no_newline { print!("{text}") } else { println!("{text}") };
 }
