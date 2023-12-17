3918af934793218b4f2cb8f5f6ebf65fd13eaa09
Renato Diniz <renatodiniz25@gmail.com>
2023-12-17 13:40:43 -0300

    Integration specs

---
+++ tests/cli.rs
@@ -0,0 +1,74 @@
+use assert_cmd::Command;
+
+#[test]
+fn with_newline_without_args() {
+  let mut cmd = Command::cargo_bin("recho").unwrap();
+
+  cmd.assert().success().stdout("\n");
+}
+
+#[test]
+fn no_newline_without_args() {
+  let mut cmd = Command::cargo_bin("recho").unwrap();
+
+  cmd.arg("-n");
+
+  cmd.assert().success().stdout("");
+}
+
+#[test]
+fn no_newline_with_args1() {
+  let mut cmd = Command::cargo_bin("recho").unwrap();
+
+  cmd.arg("-n");
+  cmd.arg("Hello");
+
+  cmd.assert().success().stdout("Hello");
+}
+
+#[test]
+fn no_newline_with_args2() {
+  let mut cmd = Command::cargo_bin("recho").unwrap();
+
+  cmd.arg("-n");
+  cmd.arg("Hello World");
+
+  cmd.assert().success().stdout("Hello World");
+}
+
+#[test]
+fn no_newline_with_args3() {
+  let mut cmd = Command::cargo_bin("recho").unwrap();
+
+  cmd.arg("-n");
+  cmd.arg("Apple pie recipe");
+
+  cmd.assert().success().stdout("Apple pie recipe");
+}
+
+#[test]
+fn with_newline_with_args1() {
+  let mut cmd = Command::cargo_bin("recho").unwrap();
+
+  cmd.arg("Hello");
+
+  cmd.assert().success().stdout("Hello\n");
+}
+
+#[test]
+fn with_newline_with_args2() {
+  let mut cmd = Command::cargo_bin("recho").unwrap();
+
+  cmd.arg("Hello World");
+
+  cmd.assert().success().stdout("Hello World\n");
+}
+
+#[test]
+fn with_newline_with_args3() {
+  let mut cmd = Command::cargo_bin("recho").unwrap();
+
+  cmd.arg("Apple pie recipe");
+
+  cmd.assert().success().stdout("Apple pie recipe\n");
+}
