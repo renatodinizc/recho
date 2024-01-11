3c232fd3bdfd0f0fbc312d62164c7c015ecde48c
Renato Diniz <renatodiniz25@gmail.com>
2024-01-11 16:36:20 -0300

    rafactor: refact code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
-use clap::{command, Arg, ArgAction};
-
+use recho::{get_args, process, Args};
 fn main() {
-    let matches = command!()
-        .arg(Arg::new("no_newline").help("Do not print newline").short('n').long("no_newline").action(ArgAction::SetTrue))
-        .arg(Arg::new("input").help("Inputs to print").default_value("").action(ArgAction::Append).trailing_var_arg(true))
-        .get_matches();
 
-    let no_newline = *matches.get_one::<bool>("no_newline").unwrap();
-    let text = &matches
-        .get_many::<String>("input")
-        .unwrap()
-        .map(|v| v.as_str())
-        .collect::<Vec<_>>().join(" ");
+    let args = get_args();
 
-    if no_newline { print!("{text}") } else { println!("{text}") };
+    process(args);
 }
